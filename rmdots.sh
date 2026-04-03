d87eae923bf3fb4a1f1fce1a7bf22e5094695de6
rlyvision <hamzamoboi@outlook.com>
2026-04-03 12:47:30 +0000

    New PKGS

---
+++ rmdots.sh
@@ -0,0 +1,1 @@
+find . -type f \( -name ".footprint" -o -name ".signature" -o -name ".md5sum" \) -delete
---
+++ updateports.sh
@@ -0,0 +1,24 @@
+for dir in */; do
+    if [ -d "$dir" ]; then
+        echo "Building in $dir..."
+        (cd "$dir" && \
+        sudo pkgmk -d && \
+        sudo pkgmk -uf && \
+	sudo pkgmk -us && \
+	sudo pkgmk -um && \
+        sudo rm -f *.tar.* stable)
+    fi
+done
+
+# Generate the ports page
+httpup-repgen .
+portspage . > index.html
+
+# Remove unwanted lines from REPO file
+if [ -f "REPO" ]; then
+    sed -i '/\.git\|updateports\.sh\|index\.html/d' REPO
+    echo "Removed lines containing .git, updateports.sh, and index.html from REPO"
+fi
+git add . 
+git commit -m " New Packages "
+git push
